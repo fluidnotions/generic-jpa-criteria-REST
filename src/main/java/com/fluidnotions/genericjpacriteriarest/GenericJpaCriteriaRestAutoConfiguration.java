929f262d435628e5ab73800a003775ea5306e679
Justin Robinson <justin.robinson@vcontractor.co.za>
2024-03-15 01:27:43 +0200

    debugging + inject into consumer OpenApi

--- src/main/java/com/fluidnotions/genericjpacriteriarest/GenericJpaCriteriaRestAutoConfiguration.java
+++ src/main/java/com/fluidnotions/genericjpacriteriarest/GenericJpaCriteriaRestAutoConfiguration.java
@@ -1,13 +1,18 @@
 package com.fluidnotions.genericjpacriteriarest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 
 @Configuration
 @AutoConfigureAfter({EntityManagerFactory.class, Jackson2ObjectMapperBuilder.class})
@@ -27,15 +32,25 @@ public class GenericJpaCriteriaRestAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = JpaCriteriaSearchService.class)
+    @ConditionalOnClass(OpenAPI.class)
+    public GroupedOpenApi libraryOpenApi() {
+        String packagesToScan = getClass().getPackage().getName();
+        return GroupedOpenApi.builder().group("REST API").packagesToScan(packagesToScan).build();
+    }
+
+    @Bean
     public JpaCriteriaSearchService jpaCriteriaSearchService(EntityManager entityManager, Jackson2ObjectMapperBuilder jacksonBuilder) {
         return new JpaCriteriaSearchService(entityManager, jacksonBuilder);
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = JpaCriteriaSearchController.class)
     public JpaCriteriaSearchController jpaCriteriaSearchController(JpaCriteriaSearchService jpaCriteriaSearchService) {
         return new JpaCriteriaSearchController(jpaCriteriaSearchService);
+    }
+
+    @Bean
+    public JpaCriteriaSearchDtoAdapterService jpaCriteriaSearchDtoAdapterService(JpaCriteriaSearchService jpaCriteriaSearchService) {
+        return new JpaCriteriaSearchDtoAdapterService(jpaCriteriaSearchService);
     }
 
 }
